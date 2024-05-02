e44f391f9f1e7c50181cd32c6b268f07544b24ca
ykrods <890082+ykrods@users.noreply.github.com>
2024-05-02 13:08:42 +0900

    Simplify

--- prerender.js
+++ prerender.js
@@ -2,18 +2,36 @@ import fs from "node:fs/promises"
 import path from "node:path"
 import { fileURLToPath } from "node:url"
 
+import { build } from 'vite'
+
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 const dirs = Object.fromEntries(
-  ["app", "ssg", "static"].map((key) => {
-    return [ key, path.resolve(__dirname, `build/${key}`) ];
-  })
+  ["app", "ssr", "static"].map(
+    (key) => [ key, path.resolve(__dirname, `build/${key}`) ]
+  )
 )
+dirs["src"] = path.resolve(__dirname, "src")
+
 
 async function main() {
+  // Step 1. build app
+  await build({
+    build: { outDir: dirs.app }
+  })
+
+  // Step 2. build src/ssr-entry.js for rendering html
+  await build({
+    build: {
+      ssr: path.join(dirs.src, "ssr-entry.js"),
+      outDir: dirs.ssr,
+    }
+  })
+
+  // Step 3. generate rendered index.html
   const template = await fs.readFile(path.join(dirs.app, "index.html"), "utf-8");
-  const { render } = await import("./build/ssg/ssg-entry.js");
+  const { render } = await import("./build/ssr/ssr-entry.js");
   const rendered = render()
   const html = template
         .replace("<!--app-head-->", rendered.head || "")
@@ -26,5 +44,7 @@ async function main() {
     html,
     { encoding: "utf-8" }
   )
+
+  console.log("output: build/static")
 }
 main()
